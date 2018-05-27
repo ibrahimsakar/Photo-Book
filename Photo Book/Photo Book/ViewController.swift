8708e106cc13b8131b8135369ce58be752c875f5
İbrahim Şakar <ibrahimsakar@Ibrahim-MacBook-Pro.local>
2018-05-27 12:55:01 +0300

    App is created from Xcode.

---
+++ Photo Book/Photo Book/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photo Book
+//
+//  Created by İbrahim Şakar on 27.05.2018.
+//  Copyright © 2018 İbrahim Şakar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
